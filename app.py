6dbc9402448b985811fcee5be1fafa130dd8d3b0
tamefowe <ftim0107@gmail.com>
2024-01-10 19:34:13 -0500

    add icon and user image

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from typing import List, Tuple
 
 from datetime import date
@@ -5,6 +7,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+BASE_DIR = Path(__file__).resolve().parent
+LOGO = os.path.join(BASE_DIR, 'icon', 'aimakr_logo.jpg') 
+USER_IMG = os.path.join(BASE_DIR, 'image', 'user.png') 
 
 def set_page_config():
     st.set_page_config(
@@ -18,11 +23,11 @@ def set_page_config():
 @st.cache_data
 def load_data() -> pd.DataFrame:
     data = pd.read_csv('data/food_hazards_data_sample.csv', encoding='latin1')
-    data['MONTH'] = pd.to_datetime(data['MONTH'], format='mixed')
+    data['TIME'] = pd.to_datetime(data['TIME'], format='mixed')
     return data
 
 def filter_by_date(data: pd.DataFrame, start_date: date, end_date:date):
-    mask = (data['MONTH'] > start_date) & (data['MONTH'] <= end_date)
+    mask = (data['TIME'] > start_date) & (data['TIME'] <= end_date)
     return data.loc[mask]
 
 def filter_data(data: pd.DataFrame, column: str, values: List[str]) -> pd.DataFrame:
@@ -33,16 +38,36 @@ def calculate_kpis(data: pd.DataFrame) -> List[float]:
     total_hazards = format(data['NUMBER OF HAZARDS'].sum(), ',d')
     return [total_hazards]
 
+
+def add_header(user):
+    """
+    # top header : user pic | name   logout    
+    """
+    user_img_name, logo = st.columns((7, 1))
+    with user_img_name:
+        col1, _, col2 = st.columns([1,1,20])
+        with col1:
+            st.image(USER_IMG, width=60)
+        with col2:
+            st.markdown(f":blue[Hello] :orange[{user.capitalize()}] :confetti_ball:")
+    with logo:
+        st.image(LOGO, width=50)
+    st.subheader('', divider='rainbow')
+    #add_space
+    for _ in range(1):
+        st.write('#')
+
+
 def display_kpi_metrics(kpis: List[float], kpi_names: List[str]):
     st.header("Food Hazards Metrics")
-    for i, (col, (kpi_name, kpi_value)) in enumerate(zip(st.columns(4), zip(kpi_names, kpis))):
+    for _, (col, (kpi_name, kpi_value)) in enumerate(zip(st.columns(4), zip(kpi_names, kpis))):
         col.metric(label=kpi_name, value=kpi_value)
 
 def display_sidebar(data: pd.DataFrame) -> Tuple[List[str], List[str], List[str]]:
     st.sidebar.header("Filters")
     st.sidebar.divider()
-    start_date = pd.Timestamp(st.sidebar.date_input("Start date", data['MONTH'].min().date()))
-    end_date = pd.Timestamp(st.sidebar.date_input("End date", data['MONTH'].max().date()))
+    start_date = pd.Timestamp(st.sidebar.date_input("Start date", data['TIME'].min().date()))
+    end_date = pd.Timestamp(st.sidebar.date_input("End date", data['TIME'].max().date()))
 
     st.sidebar.divider()
     hazard_types = sorted(data['HAZARD TYPE'].unique())
@@ -54,10 +79,10 @@ def display_charts(data: pd.DataFrame):
     combine_hazard_types = st.checkbox("Combine Hazards", value=True)
 
     if combine_hazard_types:
-        fig = px.area(data, x='MONTH', y='NUMBER OF HAZARDS',
+        fig = px.area(data, x='TIME', y='NUMBER OF HAZARDS',
                       title="Food Safety Hazards Over Time", width=900, height=500)
     else:
-        fig = px.area(data, x='MONTH', y='NUMBER OF HAZARDS', color='HAZARD TYPE',
+        fig = px.area(data, x='TIME', y='NUMBER OF HAZARDS', color='HAZARD TYPE',
                       title="Food Safety Hazards Over Time", width=900, height=500)
 
     fig.update_layout(margin=dict(l=20, r=20, t=50, b=20))
@@ -66,9 +91,12 @@ def display_charts(data: pd.DataFrame):
     st.plotly_chart(fig, use_container_width=True)
 
 
-def main():
+def main(username):
     set_page_config()
     data = load_data()
+
+    add_header(user=username)
+
     st.title("📊 Food Safety - Global Food Solutions Inc.")
 
     start_date, end_date, selected_hazard_types = display_sidebar(data)
@@ -88,4 +116,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    user = 'mike'
+    main(user)
